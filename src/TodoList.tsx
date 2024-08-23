6a31092163a2257ac5b98343b10f48b31e5eebd0
Barış Gündüz <baris@gunduzmedya.com>
2024-08-23 17:19:44 +0300

    New features with new styles

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,38 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { DeleteOutlined } from '@ant-design/icons';
+import { useState, useEffect } from "react";
+import { DeleteOutlined, FieldTimeOutlined } from '@ant-design/icons';
 import { Flex, Layout, Input, Button } from 'antd';
 
-const { Header, Footer, Content } = Layout;
+const { Header, Content } = Layout;
 const { Search } = Input;
-
-const headerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    height: 64,
-    paddingInline: 48,
-    lineHeight: '64px',
-    backgroundColor: "#ffffff"
-};
-
-const contentStyle: React.CSSProperties = {
-    textAlign: 'center',
-    backgroundColor: "#ffffff"
-};
-
-const todoTextStyle: React.CSSProperties = {
-    width: '100%',
-    textAlign: 'start',
-    lineHeight: '20px'
-}
-
-const footerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    backgroundColor: "#c9c9c9"
-};
-
-const layoutStyle = {
-    borderRadius: 8,
-    overflow: 'hidden',
-};
 
 function TodoList() {
     const [todos, setTodos] = useState(() => {
@@ -51,25 +22,26 @@ function TodoList() {
 
     const handleAddTodo = () => {
         if (inputValue.trim() !== "") {
-            setTodos([...todos, inputValue]);
+            const todoDate = new Date().toLocaleString( );
+            
+            setTodos([[inputValue, todoDate], ...todos]);
             setInputValue("");
         }
     };
 
     const handleDeleteTodo = (index: any) => {
-        setTodos(todos.filter((_: any, i: any) => i !== index));
+        setTodos(todos.filter((_: any, todo: any) => todo !== index));
     };
 
     return (
         <div>
             <Flex gap="middle" wrap>
-                <Layout style={layoutStyle}>
+                <Layout className="todoLayout">
                     <Header className="todoHeader">Todo List</Header>
-                    <Content style={contentStyle}>
-                        <div>
+                    <Content className="todoContent">
+                        <div className="todoInputText">
                             <Search
-                                placeholder="input search text"
-                                allowClear
+                                placeholder="What will you do?"
                                 enterButton="Add"
                                 onSearch={handleAddTodo}
                                 value={inputValue}
@@ -82,9 +54,13 @@ function TodoList() {
                             />
                         </div>
                         {todos.map((todo: any, index: any) => (
-                            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', minHeight: '45px' }} key={index}>
-                                <span style={todoTextStyle}>{todo}</span>
-                                <Button type="primary" danger icon={<DeleteOutlined />} size={'small'} onClick={() => handleDeleteTodo(index)} />
+                            <div className="todoListItem" key={index}>
+                                <FieldTimeOutlined className="todoListItemIcon" />
+                                <div className="todoListItemContainer">
+                                    <span className="todoText">{todo[0]}</span>
+                                    <span className="todoDate">{todo[1]}</span>
+                                </div>
+                                <Button className="todoDeleteButton" type="primary" danger icon={<DeleteOutlined />} size={'small'} onClick={() => handleDeleteTodo(index)} />
                             </div>
                         ))}
                     </Content>
